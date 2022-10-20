c5db8a79e38811075c28ee6d14c326985e807a72
totory <76061068+gemmaacaballero@users.noreply.github.com>
2022-10-20 09:39:58 +0200

    Add files via upload

---
+++ entornos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace entornos
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hola , inserte su nombre a continuacion ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("hola " + userName);
+        }
+    }
+}
